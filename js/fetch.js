4629195e7361c0b15d433a4e563eb0712aa4eedf
Fedhira Syaila <pedirasyaila@gmail.com>
2023-03-31 16:12:37 +0700

    latihann6

--- js/fetch.js
+++ js/fetch.js
@@ -1,12 +1,12 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "table.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://fedhirasyaila.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
     results.forEach(isiRow);
-}
+} 
 function isiRow(value) {
     let content = 
     isiTabel.replace("#NAMA#", value.biodata.nama)
