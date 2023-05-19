7dbb48b94767c83fa6c61890f6edd0e70b5f6460
Fedhira Syaila <pedirasyaila@gmail.com>
2023-05-19 15:00:18 +0700

    tugas10

---
+++ js/config/url_get_detail.js
@@ -0,0 +1,6 @@
+//Mendapatkan parameter dari URL
+
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get('presensiId');
+
+export let urlFetch = 'https://fedhirasyaila.herokuapp.com/presensi' + presensiId;
